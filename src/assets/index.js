47ba560d64c9a4f7d3a109be014a5a9026adb9e2
Asad Ali <asad.ali@piecyfer.com>
2023-09-07 12:50:24 +0500

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,10 @@
+import onlineDoc from "./OnlineDoc-Mockup.png";
+import sDash from './SDASH-Mockup.png';
+import profileImage from "./Shana_Profile--1.png";
+import fireProject from "./fireProject.svg"
+export {
+    onlineDoc,
+    sDash,
+    profileImage,
+    fireProject
+}
---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import {fireProject, onlineDoc, sDash} from "../assets/index.js";
+
+
+export const data = [
+  {
+    id: 1,
+    img: onlineDoc,
+    title: "OnlineDoc",
+    desc: "",
+    link: "https://www.onlinedoc.dk/auth/login",
+    git: false,
+  },
+  {
+    id: 2,
+    img: sDash,
+    title: "S-Dash",
+    link: "https://sdasha.com.au/",
+    git: false,
+  },
+  {
+    id: 3,
+    img: fireProject,
+    title: "EgileElementsFire",
+    desc: "",
+    link: "https://agilelements-prod.firebaseapp.com/",
+    git: true,
+  }
+  
+];
